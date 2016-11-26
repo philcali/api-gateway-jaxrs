9d2614207acfdea9b067a4be9ca23da7a9ea58e2
Philip Cali <philip.cali@gmail.com>
2016-11-25 21:49:41 -0500

    Adding a application builder helper.

--- src/main/java/me/philcali/api/gateway/jaxrs/FullHttpLamdaHandler.java
+++ src/main/java/me/philcali/api/gateway/jaxrs/FullHttpLamdaHandler.java
@@ -1,14 +1,79 @@
 package me.philcali.api.gateway.jaxrs;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import javax.ws.rs.core.Application;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import me.philcali.api.gateway.jaxrs.exception.ResourceException;
 import me.philcali.api.gateway.jaxrs.reflection.ReflectionResourceIndex;
 
 public abstract class FullHttpLamdaHandler implements RequestHandler<FullHttpRequest, FullHttpResponse> {
+    protected static class ApplicationBuilder {
+        private Set<Class<?>> resourceClasses;
+        private Set<Object> singletons;
+        private Map<String, Object> properties;
+
+        public ApplicationBuilder() {
+            resourceClasses = new HashSet<>();
+            singletons = new HashSet<>();
+            properties = new HashMap<>();
+        }
+
+        public ApplicationBuilder addResource(Class<?> resource) {
+            resourceClasses.add(resource);
+            return this;
+        }
+
+        public ApplicationBuilder addResource(Object resource) {
+            singletons.add(resource);
+            return this;
+        }
+
+        public ApplicationBuilder registerConfig(Object config) {
+            Arrays.stream(config.getClass().getDeclaredFields()).forEach(field -> {
+                try {
+                    field.setAccessible(true);
+                    properties.put(field.getName(), field.get(config));
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    throw new ResourceException(e);
+                }
+            });
+            return this;
+        }
+
+        public ApplicationBuilder registerConfig(String property, Object value) {
+            properties.put(property, value);
+            return this;
+        }
+
+        public Application build() {
+            return new Application() {
+                @Override
+                public Set<Class<?>> getClasses() {
+                    return Collections.unmodifiableSet(resourceClasses);
+                }
+
+                @Override
+                public Set<Object> getSingletons() {
+                    return Collections.unmodifiableSet(singletons);
+                }
+
+                @Override
+                public Map<String, Object> getProperties() {
+                    return Collections.unmodifiableMap(properties);
+                }
+            };
+        }
+    }
 
     protected abstract Application getApplication();
 
