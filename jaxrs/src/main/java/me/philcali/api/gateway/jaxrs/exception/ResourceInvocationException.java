6c700716f75570fb05299a902fb98404d64dbf00
Philip Cali <philip.cali@gmail.com>
2017-02-05 19:44:43 -0500

    Fixing minor things.

--- jaxrs/src/main/java/me/philcali/api/gateway/jaxrs/exception/ResourceInvocationException.java
+++ jaxrs/src/main/java/me/philcali/api/gateway/jaxrs/exception/ResourceInvocationException.java
@@ -1,6 +1,11 @@
 package me.philcali.api.gateway.jaxrs.exception;
 
 public class ResourceInvocationException extends ResourceException {
+    /**
+     *
+     */
+    private static final long serialVersionUID = -3514631751830659232L;
+
     public ResourceInvocationException(Throwable t) {
         super(t);
     }
